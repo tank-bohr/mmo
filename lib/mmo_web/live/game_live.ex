d052426e6fcc6e1e137e6e9395dbc12ed4f7a283
Alexey Nikitin <tank@bohr.su>
2020-10-26 19:06:12 +0300

    Remove debug logging

--- lib/mmo_web/live/game_live.ex
+++ lib/mmo_web/live/game_live.ex
@@ -8,38 +8,33 @@ defmodule MmoWeb.GameLive do
 
   @impl true
   def mount(params, _session, socket) do
-    name = params["name"] || Superhero.name
+    name = params["name"] || Superhero.name()
     :ok = GameServer.connect(name)
     {:ok, assign(socket, name: name, world: [])}
   end
 
   @impl true
   def handle_event("action", %{"key" => "ArrowUp"}, socket) do
-    Logger.debug("up")
     :ok = GameServer.move_hero(socket.assigns.name, :up)
     {:noreply, socket}
   end
 
   def handle_event("action", %{"key" => "ArrowDown"}, socket) do
-    Logger.debug("down")
     :ok = GameServer.move_hero(socket.assigns.name, :down)
     {:noreply, socket}
   end
 
   def handle_event("action", %{"key" => "ArrowLeft"}, socket) do
-    Logger.debug("left")
     :ok = GameServer.move_hero(socket.assigns.name, :left)
     {:noreply, socket}
   end
 
   def handle_event("action", %{"key" => "ArrowRight"}, socket) do
-    Logger.debug("right")
     :ok = GameServer.move_hero(socket.assigns.name, :right)
     {:noreply, socket}
   end
 
   def handle_event("action", %{"key" => "Enter"}, socket) do
-    Logger.debug("attack")
     :ok = GameServer.attack(socket.assigns.name)
     {:noreply, socket}
   end
